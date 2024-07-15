cc14fcf755caa46db569b1fa67d8f8c773894a37
dineshEth <dinesheth41@gmail.com>
2024-07-15 22:18:46 +0530

    add express middleware

--- src/app.js
+++ src/app.js
@@ -10,7 +10,18 @@ app.use(cors({
     // credentials: true,
 }));
 
+//* enable accepts json data
+//* default disabled
+app.use(express.json({
+    limit: "50kb",
+}))
 
+//* enable accepts urlencoded data (data from url)
+//* default disabled
+app.use(express.urlencoded({
+    limit:"50kb",
+    extended:true
+}));
 
 
 export default app;
