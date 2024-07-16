24c1d7d3c184b531b8ebf68dc9ca0602c95d3f77
dineshEth <dinesheth41@gmail.com>
2024-07-16 13:05:19 +0530

    add a demo route

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,6 +4,13 @@ import { Router } from "express";
 //* create a router
 const userRouter = Router();
 
+//*  add routes 
+userRouter.post('/',(req,res,next)=>{
+    res.json({
+        message:"ok"
+    });
+})
+
 
 export default userRouter;
 
