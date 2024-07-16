70c5c75e01c470d8d0cec28f2d5bedca51f66835
dineshEth <dinesheth41@gmail.com>
2024-07-16 13:02:12 +0530

    add userRouter and register

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import {conf} from './config/conf.js';
+import userRouter from './router/userRouter.js';
 
 const app = express();
 
@@ -22,6 +23,10 @@ app.use(express.urlencoded({
     limit:"50kb",
     extended:true
 }));
+
+
+//* register routers
+app.use('/api/v1/users/',userRouter);  // http://localhost:3000/api/v1/users/
 
 
 export default app;
---
+++ src/router/userRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+//* create a router
+const userRouter = Router();
+
+
+export default userRouter;
+
+
