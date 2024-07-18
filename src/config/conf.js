b1352a0bcd95a3467dbe49e54e87dcf77bd59a8c
dineshEth <dinesheth41@gmail.com>
2024-07-18 09:01:10 +0530

    add cloud credentials

--- src/config/conf.js
+++ src/config/conf.js
@@ -13,6 +13,9 @@ const conf = {
     port:String(process.env.PORT),
     mongo_db_uri:String(process.env.MONGO_DB_URI),
     frontend_domain:String(process.env.FRONTEND_DOMAIN),
+    cloud_api_name:String(process.env.CLOUD_NAME),
+    cloud_api_key:String(process.env.CLOUD_API_KEY),
+    cloud_api_secret:String(process.env.CLOUD__API_SECRET),
 }
 
 //* freeze the object { ReadOnly Object }
