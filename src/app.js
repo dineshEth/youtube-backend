3f96c2ce67cad4e497d46995f21ca837add759d8
dineshEth <dinesheth41@gmail.com>
2024-07-18 09:02:03 +0530

    remove upload config

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import cors from 'cors'
 import {conf} from './config/conf.js';
 import userRouter from './router/userRouter.js';
-import multer from 'multer';
-import path from'node:path'
 
 const app = express();
 
@@ -16,31 +14,19 @@ app.use(cors({
 //* enable accepts json data
 //* default disabled
 app.use(express.json({
-    limit: "50kb",
+    limit: "100mb",
 }))
 
 //* enable accepts urlencoded data (data from url)
 //* default disabled
 app.use(express.urlencoded({
-    limit:"50kb",
+    limit:"100mb",
     extended:true
 }));
 
-//** config multer
-//* enables file upload 
-const upload = multer({
-    dest:path.resolve('./public/temp'),
-    limits:5*1024*1024, // filesize in bytes
-})
-
 
 //* register routers
-app.use('/api/v1/users/',
-    upload.fields([
-        { name:"avatar", maxCount:1},
-        { name:"coverImage", maxCount:1}
-    ]),
-    userRouter);  // http://localhost:3000/api/v1/users/
+app.use('/api/v1/users/',userRouter);  // http://localhost:3000/api/v1/users/
 
 
 export default app;
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { signup } from "../controller/user/index.js";
-
+import { upload } from "../middleware/upload.middleware.js";
 
 //* create a router
 const userRouter = Router();
 
-//*  add routes 
-userRouter.post('/',signup)
 
+
+//*  add routes 
+userRouter.post('/',
+    upload.fields([
+        { name:"avatar", maxCount:1},
+        { name:"coverImage", maxCount:1}
+    ]),
+    signup)
 
 export default userRouter;
 
