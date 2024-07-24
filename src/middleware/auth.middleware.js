dd270208d3d77e21156bb5b1c4be81c005f52b07
dineshEth <dinesheth41@gmail.com>
2024-07-25 00:04:34 +0530

    update type error in auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -14,20 +14,21 @@ import { User } from '../model/user.model.js'
 
 
 
-async function auth(req,res,next){
+async function auth(req,_,next){
     try {
-        const token = req.cookies?.acccessToken || req.headers["authentication"]?.split(" ").at(1);
+        const token = req.cookies?.accessToken || req.headers["authentication"]?.split(" ").at(1);
         if(!token) throw new ApiError(404, "User token not found");
-
+        
         const decodedToken = await jwt.verify(token,conf.access_token_secret);
-
+        
         if(!decodedToken) throw new ApiError(401, "Unauthorized request");
-
+        
         const user = await User.findById(decodedToken?._id).select("-refreshToken -email -passord");
         if(!user) throw new ApiError(404,"User not found");
         req.user = user;
         next()
     } catch (error) {
+        console.log(error)
         throw new ApiError(500,error.message || "Server Internal Error")
     }
 }
