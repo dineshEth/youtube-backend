d3aee5b43998b23b9c873d61a21e23eb8aa228dd
dineshEth <dinesheth41@gmail.com>
2024-07-27 21:36:52 +0530

    add updatePassword controller

---
+++ src/controller/user/updatePassword.js
@@ -0,0 +1,41 @@
+/**
+ * ALGORITHM
+ * get user from auth 
+ * get user oldPassword and newPassword from frontend
+ * find user from db
+ * match oldPassword with user's stored password
+ * update password with newPassword
+ * and save it 
+ * send response
+ */
+
+import { ApiError } from "../../api/error.js";
+import { ApiResponse } from "../../api/response.js";
+import { User } from "../../model/user.model.js";
+
+async function updatePassword(req,res){
+    const { oldPassword, newPassword } = req?.body;
+    const user = req.user;
+
+    if(!user) throw new ApiError(404,"User not found");
+    if(!oldPassword || !newPassword) throw new ApiError(404,"all fields are required");
+
+    const existedUser = await User.findById(user._id);
+    if(!existedUser) throw new ApiError(404,"User not found");
+    
+    const validatePassword = await existedUser.isPasswordCorrect(oldPassword);
+    if(!validatePassword) throw new ApiError(404,"user password not match");
+
+    const updatedUser = await User.findByIdAndUpdate(user._id,
+        { 
+            $set:{
+                password: newPassword
+            }
+        }, 
+        {new :true})
+        .select("-password -email -refreshToken");
+
+    return res.status(200).json(new ApiResponse(200,"password Updated",updatedUser));
+}
+
+export default updatePassword
