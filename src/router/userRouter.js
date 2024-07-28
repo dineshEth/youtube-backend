e1a8048933d0ff53b7bf2cb60bf3b816d8c23c4c
dineshEth <dinesheth41@gmail.com>
2024-07-28 08:16:34 +0530

    add user routes

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { signup } from "../controller/user/index.js";
+import { getCurrentUser, login, logout, signup, updateAvatar, updateCoverImage, updatePassword, updateUserData } from "../controller/user/index.js";
 import { upload } from "../middleware/upload.middleware.js";
+import {auth} from '../middleware/auth.middleware.js'
 
 //* create a router
 const userRouter = Router();
@@ -8,12 +9,23 @@ const userRouter = Router();
 
 
 //*  add routes 
-userRouter.post('/',
+userRouter.post('/signup',
     upload.fields([
         { name:"avatar", maxCount:1},
         { name:"coverImage", maxCount:1}
     ]),
-    signup)
+    signup);
+    
+userRouter.post('/login',login);
+
+//* secure routes
+userRouter.post('/logout',auth,logout);
+userRouter.get('/user',auth,getCurrentUser);
+userRouter.patch('/password',auth,updatePassword);
+userRouter.patch('/userdata',auth,updateUserData);
+userRouter.patch('/avatar',upload.fields([{name:"avatar",maxCount:1}]),auth,updateAvatar);
+userRouter.patch('/coverimage',upload.fields([{name:"coverImage", maxCount:1}]),auth,updateCoverImage)
+
 
 export default userRouter;
 
