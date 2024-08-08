14c11a3c79fdad9fbe4e2ca99dd47d568d0e3535
dineshEth <dinesheth41@gmail.com>
2024-08-08 08:39:47 +0530

    add like model

---
+++ src/model/like.model.js
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        video:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        likeOwner:{
+            type:Schema.Types.ObjectId,
+            ref:User
+        }
+    },
+    {
+        timestamps:false
+    }
+)
+
+export const Like = model("Like",likeSchema);
