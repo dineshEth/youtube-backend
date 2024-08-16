0ab2e7683f35d04426708812b93a9977940fad22
dineshEth <dinesheth41@gmail.com>
2024-08-16 20:40:44 +0530

    add indexjs

--- src/controller/user/index.js
+++ src/controller/user/index.js
@@ -1,4 +1,12 @@
 import signup from "./signup.js"
+import login from "./login.js" 
+import logout from "./logout.js"
+import getCurrentUser from "./getCurrentUser.js"
+import updateUserData from "./updateUserData.js"
+import updatePassword from "./updatePassword.js"
+import updateAvatar from "./updateAvatar.js"
+import updateCoverImage from "./updateCoverImage.js"
+import getUserChannelProfile from "./userProfile.js"
 
 
-export { signup }
+export { signup,login,logout,updateAvatar,updateCoverImage,updatePassword,updateUserData,getCurrentUser,getUserChannelProfile }
