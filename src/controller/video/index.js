7c85c4ec362031048887be7ebe282c0f58159cb8
dineshEth <dinesheth41@gmail.com>
2024-08-16 22:50:46 +0530

    add indexjs

---
+++ src/controller/video/index.js
@@ -0,0 +1,21 @@
+import addVideo from "./addVideo.js";
+import deleteVideo from "./deleteVideo.js";   
+import updateVideoData from "./updateVideoData.js";
+import getVideo from "./getRequests/getVideo.js";
+import getVideosForUsername from "./getRequests/getVideosForUsername.js";
+import videos from "./getRequests/videos.js";
+import likeVideo from "./likeVideo.js";
+import commentOnVideo from './commentOnVideo.js';
+import deleteComment from "./deleteComment.js";
+
+export {
+    addVideo,
+    deleteVideo,
+    updateVideoData,
+    getVideo,
+    getVideosForUsername,
+    videos,
+    likeVideo,
+    commentOnVideo,
+    deleteComment
+}
