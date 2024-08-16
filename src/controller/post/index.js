433fbb1f8ce54d7aa608d15daf1724d7c6924e98
dineshEth <dinesheth41@gmail.com>
2024-08-16 22:48:38 +0530

    add indexjs

---
+++ src/controller/post/index.js
@@ -0,0 +1,13 @@
+import createPost from './createPost.js';
+import getPost from './getPost.js';
+import getUserPosts from './getUserPosts.js';
+import updatePost from './updatePost.js';
+import deletePost from './deletePost.js';
+
+export {
+    createPost,
+    getPost,
+    getUserPosts,
+    updatePost,
+    deletePost
+}
