511185c1277eb4eddea600c44bd123ff4cd64f6d
dineshEth <dinesheth41@gmail.com>
2024-08-16 20:39:33 +0530

    add subscription router

--- src/router/subscriptionRouter.js
+++ src/router/subscriptionRouter.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { subcribeAndUnsubscribeChannel } from "../controller/subscription/index.js";
 
 const subscriptionRouter = Router()
 
-
+//! ( :id || :username ) of user.
+//? implement :username controller
+subscriptionRouter.post('/subscribe/:id',subcribeAndUnsubscribeChannel);
 
 export default subscriptionRouter;
